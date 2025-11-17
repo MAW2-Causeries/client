a1273789827f5f52df84f2963195d1b9c3365cac
Diogo Fernandes <diogosilvafernandes85@gmail.com>
2025-11-17 08:36:35 +0100

    feat: Set minimum window size for desktop platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:causeries_client/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:window_size/window_size.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Set minimum window size for desktop platforms
+  setWindowMinSize(const Size(1280, 720));
   runApp(const Causeries());
 }
