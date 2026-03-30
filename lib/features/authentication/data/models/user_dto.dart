b1283e30c90cb042df54be764be0e56e693993b5
Diogo Fernandes <diogosilvafernandes85@gmail.com>
2026-03-30 13:57:17 +0200

    refactor(auth): remove avatarUrl from UserDto and User classes

--- lib/features/authentication/data/models/user_dto.dart
+++ lib/features/authentication/data/models/user_dto.dart
@@ -4,36 +4,24 @@ class UserDto {
   final String id;
   final String email;
   final String name;
-  final String avatarUrl;
 
-  UserDto({
-    required this.id,
-    required this.email,
-    required this.name,
-    required this.avatarUrl,
-  });
+  UserDto({required this.id, required this.email, required this.name});
 
   factory UserDto.fromJson(Map<String, dynamic> json) {
     return UserDto(
       id: json['user']?['id'] as String,
       email: json['user']?['email'] as String,
       name: json['user']?['username'] as String,
-      avatarUrl: json['user']?['profile_picture_path'] as String,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'user': {
-        'id': id,
-        'email': email,
-        'username': name,
-        'profile_picture_path': avatarUrl,
-      },
+      'user': {'id': id, 'email': email, 'username': name},
     };
   }
 
   User toDomain() {
-    return User(id: id, email: email, username: name, avatarUrl: avatarUrl);
+    return User(id: id, email: email, username: name);
   }
 }
--- lib/features/authentication/domain/entities/user.dart
+++ lib/features/authentication/domain/entities/user.dart
@@ -2,12 +2,6 @@ class User {
   final String id;
   final String email;
   final String username;
-  final String avatarUrl;
 
-  User({
-    required this.id,
-    required this.email,
-    required this.username,
-    required this.avatarUrl,
-  });
+  User({required this.id, required this.email, required this.username});
 }
